efff4a328aa7b7f8e7808b0f5a2ad75c604468e8
Roshellem24 <98101425+Roshellem24@users.noreply.github.com>
2023-02-16 12:09:37 -0500

    4.2

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -4,21 +4,17 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    private Rigidbody playerRb;
-    private GameObject focalPoint;
-    public float speed = 5.0f;
+   
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
-        focalPoint = GameObject.Find("Focal Point");
+       
     }
     public float rotationSpeed;
     // Update is called once per frame
     void Update()
     {
-        float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint. transform.forward *speed* forwardInput);
+       
         
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
